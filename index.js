ffff915649a149a023f5af1ed8e7f5cc061aa65f
lucaspere <lucasrupestre@hotmail.com>
2020-06-06 00:20:17 -0300

    primeira versão do app

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
-$(document).ready(function () {
+let test = document.querySelector(".teste");
 
-   let text = '';
+let text = '';
 
-   window.onload = function () {
-      fetch("https://newsapi.org/v2/top-headlines?country=br&apiKey=c8423062c93a4db7bfa845c323fa0fd3")
-         .then(res => res.json())
-         .then((data) => {
-            data.articles.map(article => {
-               text +=
-                  `
+window.onload = function () {
+   fetch("https://newsapi.org/v2/top-headlines?country=br&apiKey=c8423062c93a4db7bfa845c323fa0fd3")
+      .then(res => res.json())
+      .then((data) => {
+         data.articles.map(article => {
+            text +=
+               `
                <div class="container1">
                   <div class="container-img">
                      <img class="img-responsive"
@@ -17,8 +17,8 @@ $(document).ready(function () {
                   </div>
                   <div class="container-body">
                      <div class="container-titles">
-                        <p class="title">${article.title}</p>
-                        <p class="info"><time datetime="2020-06-05T16:47:42Z">${new Date(article.publishedAt)}</time>- Fonte:<strong>${article.source.name}</strong></p>
+                        <p class="title"><a href="${article.url}" class="text-primary" target="_blank">${article.title}</a></p>
+                        <p class="info"><time datetime="2020-06-05T16:47:42Z">${new Date(article.publishedAt)}</time>- <strong> ${article.source.name}</strong></p>
                      </div>
                      <div class="container-text">
                         <p class="text">${article.content}</p>
@@ -26,10 +26,9 @@ $(document).ready(function () {
                   </div>
                </div>
             `
-            });
+         });
 
-            $(".teste").html(text);
+         test.innerHTML = text;
 
-         })
-   }
-});
+      })
+}
